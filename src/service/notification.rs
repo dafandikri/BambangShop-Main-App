9b1f24642dd2a53be68d3bd7b984d0fab2203a32
dafandikri <dafandikri@gmail.com>
2025-03-27 04:29:40 +0700

    fix: fix duplicate import of Notification, missin return value un route_stage(), and unused imports

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,6 +1,4 @@
 use crate::model::notification::Notification;
-use crate::model::notification::Notification;
-use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
 use bambangshop::{compose_error_response, Result};
