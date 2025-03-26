0d976156656cac99768a3c584d925d6ed8c30bdd
dafandikri <dafandikri@gmail.com>
2025-03-27 04:37:16 +0700

    fix: Update the route to match the URL pattern trying to access

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -5,7 +5,7 @@ use crate::model::subscriber::Subscriber;
 use crate::service::notification::NotificationService;
 use bambangshop::Result;
 
-#[post("/<product_type>", data = "<subscriber>")]
+#[post("/subscribe/<product_type>", data = "<subscriber>")]
 pub fn subscribe(
     product_type: &str,
     subscriber: Json<Subscriber>,
